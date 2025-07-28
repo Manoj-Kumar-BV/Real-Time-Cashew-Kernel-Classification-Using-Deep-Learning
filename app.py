52e894ad42dafc35544b049fe14614a07bd684ba
Manoj-Kumar-BV <manojbv609@gmail.com>
2025-07-28 23:04:08 +0530

    Updated UI

--- app.py
+++ app.py
@@ -244,4 +244,4 @@ if __name__ == '__main__':
         print("Please ensure 'best.pt' file is in the current directory")
     
     print("=" * 50)
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
